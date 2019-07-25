4ea61b9f6d444039b38c962e36d3dffb9ea25fe9
Danny Zeng <DANNY.ZENG@oocl.com>
2019-07-25 09:29:30 +0800

    finish ExpressRepository

---
+++ src/main/java/com/oocl/packagebooking/repository/ExpressRepository.java
@@ -0,0 +1,7 @@
+package com.oocl.packagebooking.repository;
+
+import com.oocl.packagebooking.entity.Express;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpressRepository extends JpaRepository<Express,String> {
+}
